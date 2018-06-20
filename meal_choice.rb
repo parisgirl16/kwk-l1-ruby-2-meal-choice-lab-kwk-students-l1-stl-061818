fa49a6b95cc898f44a041c3686ea420f51d427b2
parisgirl16 <parisgirl16@sbcglobal.net>
2018-06-20 13:51:44 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -9,15 +9,3 @@ def meal_choice(breakfast="frosted flakes", lunch="Grilled cheese", dinner="salm
   puts "A great choice for dinner is #{dinner}!"
 end
 meal_choice("pancakes","peanut butter","brocolli")
-
-def snacks(snack="Cheetos")
-  puts "Any time is a great time for #{snack}!"
-end
-snacks
-
-def meal_choice(breakfast="frosted flakes", lunch="Grilled cheese", dinner="salmon")
-  puts "Morning is the best time for #{breakfast}!"
-  puts "#{lunch} is a great choice for lunch!"
-  puts "A great choice for dinner is #{dinner}!"
-end
-meal_choice
